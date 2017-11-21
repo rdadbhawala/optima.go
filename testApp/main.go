a2be7b4f8dd243c2c1ac6dc2c537cbaefc0bb58a
Rushabh Dadbhawala <rushabh.dadbhawala@continuum.net>
2017-11-21 16:28:25 +0530

    removed unwanted files and commented code

--- testApp/main.go
+++ testApp/main.go
@@ -10,17 +10,6 @@ import (
 	"github.com/rdadbhawala/optima.go/optima/jobsPerSec"
 )
 
-// func main() {
-// 	jp := newSleepJobProducer(time.Millisecond * 150)
-// 	w := goroutine.NewWorkshop(90)
-// 	//w.AddWorker(4)
-// 	b := jobsPerSec.NewBalancer(w, jp)
-// 	go b.Start()
-
-// 	time.Sleep(1000 * time.Second)
-// 	fmt.Println("JP", jp.count)
-// }
-
 func main() {
 	jp := newSleepJobProducer(time.Millisecond * 150)
 	w := goroutine.NewWorkshop(90)
