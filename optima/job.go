4ab0160c53267c55bffd8dff7f73f77eaece1c07
Rushabh Dadbhawala <rushabh.dadbhawala@continuum.net>
2017-11-23 09:34:27 +0530

    Support a DoWork function

--- optima/job.go
+++ optima/job.go
@@ -9,3 +9,21 @@ type Job interface {
 type Producer interface {
 	GetNextJob() Job
 }
+
+// DoWork is a function, useful when job types are inline in code
+type DoWork func()
+
+// DoWorkInJob wraps a DoWork function in an optima.Job so that it can be used by the system
+func DoWorkInJob(d DoWork) Job {
+	return &doWorkJob{
+		dw: d,
+	}
+}
+
+type doWorkJob struct {
+	dw DoWork
+}
+
+func (d *doWorkJob) DoWork() {
+	d.dw()
+}
